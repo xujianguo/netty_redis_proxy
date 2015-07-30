15d1f520bcf8d536c55b4c8d52172b69d39b183d
xujianguo <450846733@qq.com>
2015-07-30 20:36:11 +0800

    系统启动类

---
+++ src/com/gdut/xujianguo/Main.java
@@ -0,0 +1,13 @@
+package com.gdut.xujianguo;
+
+/**
+ * 系统启动类
+ * @author xujianguo
+ * @email ray_xujianguo@yeah.net
+ * @time 2015年7月30日
+ */
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
