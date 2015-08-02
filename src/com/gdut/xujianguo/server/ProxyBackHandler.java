b29ee463e318786df4cf92f5d3ce05ae5c741d1e
xujianguo <450846733@qq.com>
2015-08-02 15:21:15 +0800

    对多个redis进行任务分配

--- src/com/gdut/xujianguo/server/ProxyBackHandler.java
+++ src/com/gdut/xujianguo/server/ProxyBackHandler.java
@@ -1,6 +1,11 @@
 package com.gdut.xujianguo.server;
 
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
 
 /**
  * 代理的后端处理
@@ -9,5 +14,42 @@ import io.netty.channel.ChannelHandlerAdapter;
  * @time 2015年8月1日
  */
 public class ProxyBackHandler extends ChannelHandlerAdapter {
+	private final Channel fontChannel;
 	
+	public ProxyBackHandler(Channel fontChannel) {
+		this.fontChannel = fontChannel;
+	}
+	
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		ctx.read();
+		ctx.write(Unpooled.EMPTY_BUFFER);
+	}
+	
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg)
+			throws Exception {
+		fontChannel.writeAndFlush(msg).addListener(new ChannelFutureListener() {
+			@Override
+			public void operationComplete(ChannelFuture future) throws Exception {
+				if(future.isSuccess()) {
+					ctx.channel().read();
+				} else {
+					future.channel().close();
+				}
+			}
+		});
+	}
+	
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		ProxyFontHandler.closeOnFlush(fontChannel);
+	}
+	
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+			throws Exception {
+		cause.printStackTrace();
+		ProxyFontHandler.closeOnFlush(ctx.channel());
+	}
 }
--- src/com/gdut/xujianguo/server/ProxyFontHandler.java
+++ src/com/gdut/xujianguo/server/ProxyFontHandler.java
@@ -1,11 +1,20 @@
 package com.gdut.xujianguo.server;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 import com.gdut.xujianguo.param.Instance;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOption;
 
 /**
  * 代理的前端处理
@@ -16,13 +25,91 @@ import io.netty.channel.ChannelHandlerContext;
 public class ProxyFontHandler extends ChannelHandlerAdapter {
 	//Redis的实例列表
 	private List<Instance> instances;
+	//实例跟Channel组成的Map
+	private volatile Map<Instance, Channel> backChannels; 
 	
 	public ProxyFontHandler(List<Instance> instances) {
 		this.instances = instances;
+		backChannels = new HashMap<Instance, Channel>();
 	}
 	
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
-		
+		final Channel fontChannel = ctx.channel();
+		for(Instance instance : instances) {
+			startInstance(fontChannel, instance);
+		}
+	}
+	
+	private void startInstance(Channel fontChannel, Instance instance) {
+		Bootstrap bootstrap = new Bootstrap();
+		bootstrap
+			.group(fontChannel.eventLoop())
+			.channel(fontChannel.getClass())
+			.handler(new ProxyBackHandler(fontChannel))
+			.option(ChannelOption.AUTO_READ, false);
+		ChannelFuture future = bootstrap.connect(instance.getAddress(), instance.getPort());
+		backChannels.put(instance, future.channel());
+		future.addListener(new ChannelFutureListener() {
+			@Override
+			public void operationComplete(ChannelFuture future) throws Exception {
+				if(future.isSuccess()) {
+					fontChannel.read();
+				} else {
+					fontChannel.close();
+				}
+			}
+		});
+	}
+	
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg)
+			throws Exception {
+		Channel selectChannel = null;
+		while(true) {
+			Channel channel = backChannels.get(instances.get(new Random().nextInt(instances.size())));
+			if(channel != null) {
+				selectChannel = channel;
+				break;
+			}
+		}
+		if(selectChannel.isActive()) {
+			selectChannel.writeAndFlush(msg).addListener(new ChannelFutureListener() {
+				@Override
+				public void operationComplete(ChannelFuture future) throws Exception {
+					if(future.isSuccess()) {
+						ctx.channel().read();
+					} else {
+						future.channel().close();
+					}
+				}
+			});
+		}
+	}
+	
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		for(Channel channel : backChannels.values()) {
+			if(channel != null) {
+				closeOnFlush(channel);
+			}
+		}
+	}
+	
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+			throws Exception {
+		cause.printStackTrace();
+		closeOnFlush(ctx.channel());
+	}
+	
+	/**
+	 * 清空缓存，然后关闭Channel
+	 * @param channel
+	 */
+	public static void closeOnFlush(Channel channel) {
+		if(channel.isActive()) {
+			channel.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+		}
 	}
 }
